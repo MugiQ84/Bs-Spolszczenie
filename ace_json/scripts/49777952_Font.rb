84904160cffd6dbcc9071adf62d8beddd5cde410
MugiQ84 <bilikkacper8@gmail.com>
2026-05-09 15:39:16 +0200

    Keep only Data, Graphics, Fonts

--- ace_json/scripts/49777952_Font.rb
+++
@@ -1,5 +0,0 @@
-Font.default_size = 22     #フォントサイズ変更
-Font.default_name = [ "Lolita",*Font.default_name] #フォント変更
-Font.default_outline = true #縁取り変更
-Font.default_bold = false # ボールド(太字)
-Font.default_italic = false # イタリック(斜め字)
